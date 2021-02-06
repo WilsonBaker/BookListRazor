a6f1746c0536fd22bfbc5325fafb0c2e2237909e
Wilson <wilsonnzaccount@outlook.com>
2021-02-06 17:49:48 +1300

    Update bookListJavaScript.js

--- wwwroot/js/bookListJavaScript.js
+++ wwwroot/js/bookListJavaScript.js
@@ -12,7 +12,24 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            { "data": "name", "width": "30%" }
+            { "data": "name", "width": "30%" },
+            { "data": "author", "width": "30%" },
+            { "data": "isbn", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                           Edit
+                        </a>
+                        &nbsp;
+                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                           Edit
+                        </a>
+</div>
+                    `
+                }
+            }
         ]
     })
 }
