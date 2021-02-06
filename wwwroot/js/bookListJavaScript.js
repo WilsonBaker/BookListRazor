702a8b852da0c383f7186343a0b56641b7c51e4d
Wilson <wilsonnzaccount@outlook.com>
2021-02-06 17:24:14 +1300

    Adding JS functionality

--- wwwroot/js/bookListJavaScript.js
+++ wwwroot/js/bookListJavaScript.js
@@ -3,3 +3,16 @@
 $(document).ready(function () {
     loadDataTable();
 })
+
+function loadDataTable() {
+    dataTable = $('#DT_load').dataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "30%" }
+        ]
+    })
+}
